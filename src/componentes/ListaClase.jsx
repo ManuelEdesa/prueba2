ba9b7001e5741ee48f825af5ae6a17a1427000cc
ManuelEdesa <war4zone@gmail.com>
2021-12-20 14:02:36 +0100

    1 file added

---
+++ src/componentes/ListaClase.jsx
@@ -0,0 +1,14 @@
+function {
+  const listaInicial = [];
+  if (props.elementos !== undefined) {
+    for (let i = 0; i < props.elementos.length; i++) {
+      listaInicial.push(
+        <ComponenteListaClase
+          done={props.elementos[i].done}
+          texto={props.elementos[i].texto}
+          prioridad={props.elementos[i].prioridad}
+        />
+      );
+    }
+  }
+}
