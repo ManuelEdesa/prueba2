038c06457ae1e81cff4e93f54b2a2945cc2b60a5
ManuelEdesa <war4zone@gmail.com>
2022-01-11 11:39:10 +0100

    2 files modified, 1 added

--- src/components/Perfil.jsx
+++ src/components/Perfil.jsx
@@ -4,6 +4,7 @@ export function Perfil() {
   return (
     <div>
       <h1>Página de perfil</h1>
+      <h2>Aqui va la información del perfil</h2>
     </div>
   );
 }
---
+++ src/components/data/MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItems =[
+  {
+    id:1,
+    path: '/',
+    title: 'Home',
+  },
+  {
+    id:2,
+    path: '/listas',
+    title: 'Listas de tareas',
+  },
+  {
+    id:3,
+    path: '/perfil',
+    title: 'Perfil',
+  },
+]
