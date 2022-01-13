cf3b9d16c3df3ddc39a1da856f1febb574dd1c9b
ManuelEdesa <war4zone@gmail.com>
2022-01-13 10:11:18 +0100

    1 file modified

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,7 @@ import { Home } from './components/Home';
 import { PaginaListas } from './components/PaginaListas';
 import { Perfil } from './components/Perfil';
 import Header from './components/Header';
-import { MenuItems } from './components/data/MenuItems';
-
-{/* */}
-function createRoutes (){
-  //creamos la lista de rutas
-//creamos una variable auxiliar
-  const listRoutes=[];
-//creamos el bucle para recorrer las rutas que nos devuelven 
-for (let i=0;i<MenuItems.length; i++){
-listRoutes.push(
-  <Route path={MenuItems[i].path}
-   exact
-   component={MenuItems[i].component}/>
-);
-} 
-  return listRoutes;
-}
+import { MenuItems } from '/data/MenuItems';
 
 export function App() {
   return (
